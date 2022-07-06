a1281b30d6862d5ced6459c5fde222893f27c44e
HiraMohammedK <98226271+HiraMohammedK@users.noreply.github.com>
2022-07-06 18:52:23 +0530

    Add files via upload

---
+++ linear_search.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+int Linear_Search(int data[],int N,int item)
+{
+    int pos;
+    for(pos=1;pos<N;pos++)
+    {
+    	if (data[pos] == item)
+    	{
+    		 
+    		return pos;
+    	}
+    }
+   return -1;
+
+}
+int main()
+{
+ int k,n;
+ int choice=1;
+ do
+ {
+    int count=0;
+ int *data;
+ cout<<"Enter the number of elements of the array: ";
+ cin>>n;
+ cout<<"Enter the elements of the array: ";
+  for(count=0; count<n; count++)
+  {
+      cin>>data[count];
+  }
+ cout<<"Enter element to search: ";
+ cin>>k;
+ int loc = Linear_Search(data,n,k);
+ if (loc!=-1)
+ {
+		cout<<"Element "<<k <<" present at position "<<loc+1 <<endl;
+ }
+ else
+ {
+		cout<<k<<" is not present in the given array"<<endl;
+ }
+ cout<<"\n";
+ return 0;
+ cout<<"Do you want to continue?"<<endl;
+ cout<<"1:Yes\n2:No\nEnter your choice: ";
+ cin>>choice;
+ }while(choice!=2);
+
+}
